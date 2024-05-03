2eee2878bfe25da2abac3770dac074b627f9ca46
Maksim Volkmann <mvolkman@2-H-9.42heilbronn.de>
2024-05-03 15:02:28 +0200

    testing gettimeofday function

--- philo.c
+++ philo.c
@@ -6,11 +6,31 @@
 /*   By: mvolkman <mvolkman@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 10:29:06 by mvolkman          #+#    #+#             */
-/*   Updated: 2024/05/03 10:29:19 by mvolkman         ###   ########.fr       */
+/*   Updated: 2024/05/03 15:02:10 by mvolkman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
-{
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h> // for sleep function
 
+int main() {
+    struct timeval tv; // Declare a variable of type struct timeval
+
+    // Get the current time
+    gettimeofday(&tv, NULL);
+
+    // Convert seconds to milliseconds
+    long milliseconds_from_seconds = tv.tv_sec * 1000;
+
+    // Add milliseconds from microseconds
+    long milliseconds_from_microseconds = (tv.tv_usec / 1000);
+
+    // Add milliseconds from seconds and microseconds
+    long total_milliseconds = milliseconds_from_seconds + milliseconds_from_microseconds;
+
+    printf("Current time in milliseconds (from seconds): %ld\n", milliseconds_from_seconds);
+    printf("Current time in milliseconds (from microseconds): %ld\n", total_milliseconds);
+
+    return 0;
 }
