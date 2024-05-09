e4480f9d0aeb10d88da3e69653808f7e3e729052
Maksim Volkmann <mvolkman@2-H-9.42heilbronn.de>
2024-05-09 12:24:53 +0200

    feat: check if passed arguments are numeric

---
+++ Makefile
@@ -0,0 +1,55 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mvolkman <mvolkman@student.42heilbronn.    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/05/09 10:52:21 by mvolkman          #+#    #+#              #
+#    Updated: 2024/05/09 10:54:03 by mvolkman         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -g
+
+SRC = \
+	philo.c \
+
+OBJ = $(SRC:.c=.o)
+
+INCLUDES = -I./include
+
+NAME = philo
+
+GREEN = \033[0;32m
+BLUE = \033[0;34m
+ORANGE = \033[38;5;214m
+RED = \033[0;31m
+YELLOW = \033[0;33m
+WHITE = \033[0;37m
+
+# Rules
+all: $(NAME)
+	@echo "$(GREEN)$(NAME) compiled successfully!$(WHITE)"
+
+$(NAME): $(OBJ)
+	@echo "$(BLUE)Building $(NAME)$(WHITE)"
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJ) $(INCLUDES)
+	@echo "$(GREEN)$(NAME) created successfully!$(WHITE)"
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@ $(INCLUDES)
+	@echo "$(GREEN)Compiled $< successfully!$(WHITE)"
+
+clean:
+	@echo "$(RED)Cleaning object files...$(WHITE)"
+	rm -f $(OBJ)
+
+fclean: clean
+	@echo "$(RED)Cleaning binaries...$(WHITE)"
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: mvolkman <mvolkman@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 10:29:06 by mvolkman          #+#    #+#             */
-/*   Updated: 2024/05/09 10:21:39 by mvolkman         ###   ########.fr       */
+/*   Updated: 2024/05/09 12:22:40 by mvolkman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -212,13 +212,16 @@ void	initialize(t_data *data)
 	data->time_to_sleep = 0;
 }
 
+//./philo 10 450 200 200 2
+
 void parse_input_data(t_data *data, int ac, char **av)
 {
 	data->num_of_philos = ft_atoi(av[1]);
 	data->time_to_die = ft_atoi(av[2]);
 	data->time_to_eat = ft_atoi(av[3]);
 	data->time_to_sleep = ft_atoi(av[4]);
-	if (ac == 6) {
+	if (ac == 6)
+	{
 		data->num_of_meals_required = ft_atoi(av[5]);
 	}
 }
@@ -235,8 +238,38 @@ void initialize_data(t_data *data)
 		pthread_create(&data->tid[i], NULL, routine, NULL);
 		i++;
 	}
-	while (i < data->num_of_philos) {
+	while (i < data->num_of_philos)
+	{
 		pthread_mutex_init(&data->forks[i], NULL);
+		i++;
+	}
+}
+
+int	is_numeric(char *str)
+{
+	if(*str == '\0')
+		return (0);
+	while(*str)
+	{
+		if(*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+	return 1;
+}
+
+void	check_if_numbers(int ac, char **av)
+{
+	int	i;
+
+	i = 1;
+	while(i < ac)
+	{
+		if(!is_numeric(av[i]))
+		{
+			printf("Error: All arguments must be numeric!\n");
+			exit(EXIT_FAILURE);
+		}
 		i++;
 	}
 }
@@ -244,9 +277,10 @@ void initialize_data(t_data *data)
 int	main(int ac, char **av)
 {
 
-	t_data data;
-	initialize(&data);
+	t_data	data;
 
+	check_if_numbers(ac, av);
+	initialize(&data);
 	if(ac != 5 && ac != 6)
 	{
 		instructions();
@@ -259,9 +293,9 @@ int	main(int ac, char **av)
 
 	}
 	printf("Number of Philosophers: %d\n", data.num_of_philos);
-	printf("Time to Die: %d milliseconds\n", data.time_to_die);
-	printf("Time to Eat: %d milliseconds\n", data.time_to_eat);
-	printf("Time to Sleep: %d milliseconds\n", data.time_to_sleep);
+	printf("Time to Die: %llu milliseconds\n", data.time_to_die);
+	printf("Time to Eat: %llu milliseconds\n", data.time_to_eat);
+	printf("Time to Sleep: %llu milliseconds\n", data.time_to_sleep);
 	printf("Number of Times Each Philosopher Must Eat: %d\n", data.num_of_meals_required);
 
 	int i = 0;
@@ -272,7 +306,5 @@ int	main(int ac, char **av)
 	}
 
 	free(data.tid);
-
-
 	return (0);
 }
